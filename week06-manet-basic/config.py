84de5bf69e4ab8565b006a1a78e1730b8d558b69
chidchanok23 <chidchanok.cha@kkumail.com>
2026-03-31 04:00:13 +0700

    update3

--- week06-manet-basic/config.py
+++ week06-manet-basic/config.py
@@ -1,8 +1,9 @@
 #Step 0: Shared Configuration
 # config.py
+import sys
+
 HOST = "127.0.0.1"
-BASE_PORT = 7000
+ALL_PORTS = [7000, 7001, 7002]
 BUFFER_SIZE = 1024
-NEIGHBORS = [7001, 7002]  # Example peer ports
 FORWARD_PROBABILITY = 0.5  # 50% chance to forward
 TTL = 3  # Max hops for message
